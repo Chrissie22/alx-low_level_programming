8b44207eea25d657c893af4836a9bc95cd9cda82
Chrissie22 <christabel.ojobolo@gmail.com>
2023-09-07 15:49:32 -0700

    prints a string using write function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - this prints a string
+ * Followed by a new line
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
