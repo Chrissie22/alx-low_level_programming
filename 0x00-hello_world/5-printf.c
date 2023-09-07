4091c3000b414461ddd0f7619133348d48f9867b
Chrissie22 <christabel.ojobolo@gmail.com>
2023-09-07 14:32:47 -0700

    prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string
+ * followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
