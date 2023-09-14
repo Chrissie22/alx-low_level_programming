1d21deff56a8fdb979e7108cd746f086130252e3
Chrissie22 <christabel.ojobolo@gmail.com>
2023-09-14 08:49:53 -0700

    functions header file

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* _MAIN_H_ */
