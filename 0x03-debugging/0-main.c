5fc78c108bb676cd9cdc3ccf3866aca1b77e0f76
Chrissie22 <christabel.ojobolo@gmail.com>
2023-09-14 18:03:07 -0700

    Fix bug

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - checks the code
+ *
+ * Return: Always 0 upon (Success).
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
