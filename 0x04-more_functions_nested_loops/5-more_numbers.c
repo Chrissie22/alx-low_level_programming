cf9bae66bb4121bfeb0a459364cd2c65362c1edc
Chrissie22 <christabel.ojobolo@gmail.com>
2023-09-15 16:09:09 -0700

    prints numbers from 0 to 14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,10 +15,9 @@ void more_numbers(void)
 		for (n = 0; n <= 14; n++)
 		{
 			if (n > 9)
-				_putchar((n /10) + '0);
-			_putchar((n % 10) + '0);
+				_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
 		}
 		_putchar('\n');
 	}
 }
-
