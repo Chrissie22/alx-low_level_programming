2c53fec3d875e1316d925bcb104db638dfda118e
Chrissie22 <christabel.ojobolo@gmail.com>
2023-09-15 16:52:39 -0700

    prime factors

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,32 +1,26 @@
 #include <stdio.h>
+#include <math.h>
 
 /**
- * main - Prints from 1 - 100, but prints 'Fizz' for multiples of 3,
- * and prints 'Buzz' for multiples of 5 and 'FizzBuzz' for both
- *
- * Return: Always 0 upon (Success).
- */
+  * main - Prints the largest prime factor of the number 612852475143
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
-	int i;
+	long x, maxf;
+	long number = 612852475143;
+	double square = sqrt(number);
 
-	for (i = 1; i <= 100; i++)
+	for (x = 1; x <= square; x++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
-			printf("FizzBuzz ");
-		else if (i % 3 == 0)
-			printf("Fizz ");
-		else if (i % 5 == 0)
+		if (number % x == 0)
 		{
-			if (i != 100)
-				printf("Buzz ");
-			else
-				printf("Buzz");
+			maxf = number / x;
 		}
-		else
-			printf("%d ", i);
 	}
-	printf("\n");
+
+	printf("%ld\n", maxf);
 
 	return (0);
 }
