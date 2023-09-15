b541c9019cc7b8a0a3a358e218a40292640a7f61
Chrissie22 <christabel.ojobolo@gmail.com>
2023-09-15 15:58:29 -0700

    prints numbers from 0 to 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main"
+
+/**
+ * more_numbers - print numbers multiple times 0 - 14
+ * Foloowed by a new line
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, n;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+				_putchar((n /10) + '0);
+			_putchar((n % 10) + '0);
+		}
+		_putchar('\n');
+	}
+}
+
