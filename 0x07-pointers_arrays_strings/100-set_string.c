3454e55b7a0d53277a37a7207a89b7d6f3cc9221
Chrissie22 <christabel.ojobolo@gmail.com>
2023-09-25 14:32:48 -0700

    sets the value of a pointer to a chair

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: string
+ * @to: string to set
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
