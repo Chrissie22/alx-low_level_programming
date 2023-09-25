ad95dde10a4e6bdec3e99da1c153b150c6b5f1ac
Chrissie22 <christabel.ojobolo@gmail.com>
2023-09-25 14:30:26 -0700

    Header file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,6 +8,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif /* _MAIN_H_ */
