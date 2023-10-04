de852b205e97553c31209c6c57935d4b99513105
Chrissie22 <christabel.ojobolo@gmail.com>
2023-10-04 04:42:44 -0700

    Allocates nth size of char and initializes it to a special character

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -24,7 +24,7 @@ char *create_array(unsigned int size, char c)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; ptr[i] != '\0'; i++)
+	for (i = 0; i < size; i++)
 		ptr[i] = c;
 
 	return (ptr);
