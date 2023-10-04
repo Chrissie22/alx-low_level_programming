32ae7ec4ca00b6fa8485c14a179a739b8a1ff6e8
Chrissie22 <christabel.ojobolo@gmail.com>
2023-10-04 05:48:21 -0700

    concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -27,3 +27,16 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	i = ci = 0;
 	while (s1[i] != '\0')
+	{
+		conct[i] = s1[i];
+		i++;
+	}
+
+	while (s2[ci] != '\0')
+	{
+		conct[i] = s2[ci];
+		i++, ci++;
+	}
+	conct[i] = '\0';
+	return (conct);
+}
