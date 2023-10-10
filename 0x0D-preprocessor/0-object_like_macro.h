89717e575d03d54d9c732d2b309cf1ec57d56ec7
Chrissie22 <christabel.ojobolo@gmail.com>
2023-10-10 12:19:53 -0700

    a header macro named Size

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,15 @@
+#ifndef _OBJECT_LIKE_MACRO_H
+#define _OBJECT_LIKE_MACRO_H
+
+/**
+ * SIZE - Macro to define the size of a buffer or array as 1024.
+ *
+ * Description:
+ * This macro defines the size of a buffer or array as 1024. It is used to set
+ * a constant value that can be used throughout the program. Any occurrence of
+ * SIZE in the code will be replaced with the value
+ * 1024 during the preprocessing stage.
+ */
+#define SIZE 1024
+
+#endif /* _OBJECT_LIKE_MACRO_H */
