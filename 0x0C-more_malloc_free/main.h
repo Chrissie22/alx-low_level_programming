199fca50ec831e6e98a5f33ac9d3880bfd3f90f0
Chrissie22 <christabel.ojobolo@gmail.com>
2023-11-23 12:16:02 -0800

    header File

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,9 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif /* _MAIN_H_ */
