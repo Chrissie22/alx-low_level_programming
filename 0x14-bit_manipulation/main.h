985f9365798e38ad2e01daaeca9666dde1dd4f44
Chrissie <christabelojobolo@yahoo.com>
2023-12-14 14:51:27 -0800

    Header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,5 +7,7 @@ int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
 #endif /* _MAIN_H_ */
